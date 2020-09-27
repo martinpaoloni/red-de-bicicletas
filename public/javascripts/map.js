312a202281473f53e85210e9070734206a114d7f
Martin Paoloni <martinpaoloni@gmail.com>
2020-09-27 20:27:53 -0300

    Modificado el mapa con algunas ubicaciones de Quilmes

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,9 +1,9 @@
-var map = L.map('main_map').setView([-34.6012424, -58.3861497], 13);
+var map = L.map('main_map').setView([-34.724225, -58.2817394], 13); // Quilmes
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-34.6012424, -58.3861497]).addTo(map);
-L.marker([-34.596932, -58.3808287]).addTo(map);
-L.marker([-34.599564, -58.3778777]).addTo(map);
+L.marker([-34.7253769, -58.2515929]).addTo(map); // Brandsen y Mitre
+L.marker([-34.7198556, -58.2556818]).addTo(map); // Alsina y Mitre
+L.marker([-34.7203426, -58.2566075]).addTo(map); // Alsina y Alvear
