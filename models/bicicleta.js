0d7cc244d69cff067712822c22839b80861688eb
Martin Paoloni <martinpaoloni@gmail.com>
2020-09-27 20:59:16 -0300

    api de bicicletas

--- models/bicicleta.js
+++ models/bicicleta.js
@@ -33,8 +33,8 @@ Bicicleta.removeById = function(aBiciId){
 }
 
 
-var a = new Bicicleta(1, 'rojo', 'urbana', [-34.6012424, -58.3861497]);
-var b = new Bicicleta(2, 'blanca', 'urbana', [-34.596932, -58.3808287]);
+var a = new Bicicleta(1, 'rojo', 'urbana', [-34.7253769, -58.2515929]); // Brandsen y Mitre
+var b = new Bicicleta(2, 'blanca', 'urbana', [-34.7198556, -58.2556818]); // Alsina y Mitre
 
 Bicicleta.add(a);
 Bicicleta.add(b);
--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -4,6 +4,14 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-34.7253769, -58.2515929]).addTo(map); // Brandsen y Mitre
-L.marker([-34.7198556, -58.2556818]).addTo(map); // Alsina y Mitre
-L.marker([-34.7203426, -58.2566075]).addTo(map); // Alsina y Alvear
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        });
+    }
+
+});
