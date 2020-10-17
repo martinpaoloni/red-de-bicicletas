8888bbb7080a82aafbdd6ff689198ea0c12231a1
Martin Paoloni <martinpaoloni@gmail.com>
2020-10-17 20:16:27 -0300

    Seguridad para API de bicicletas

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const passport = require('./config/passport');
 const session = require('express-session');
-
-const Usuario = require('./models/usuario');
-const Token = require('./models/token');
-
+const jwt = require('jsonwebtoken');
 
 var indexRouter = require('./routes/index');
 var usuariosRouter = require('./routes/usuarios');
@@ -16,11 +13,18 @@ var tokenRouter = require('./routes/token');
 var bicicletasRouter = require('./routes/bicicletas');
 var bicicletasAPIRouter = require('./routes/api/bicicletas');
 var usuariosAPIRouter = require('./routes/api/usuarios');
+//var authAPIRouter = require('./routes/api/auth');
+
+const Usuario = require('./models/usuario');
+const Token = require('./models/token');
 
 
 const store = new session.MemoryStore;
 
 var app = express();
+
+app.set('secretKey', 'jwt_pwd_!!223344');
+
 app.use(session({
   cookie: { maxAge: 240 * 60 * 60 * 100 }, // 10 dias
   store: store,
@@ -125,7 +129,7 @@ app.use('/usuarios', usuariosRouter);
 app.use('/token', tokenRouter);
 
 app.use('/bicicletas', loggedIn, bicicletasRouter);
-app.use('/api/bicicletas', bicicletasAPIRouter);
+app.use('/api/bicicletas', validarUsuario, bicicletasAPIRouter);
 app.use('/api/usuarios', usuariosAPIRouter);
 
 // catch 404 and forward to error handler
@@ -152,5 +156,20 @@ function loggedIn(req, res, next) {
     res.redirect('/login');
   }
 };
+
+function validarUsuario(req, res, next) {
+  jwt.verify(req.headers['x-access-token'], req.app.get('secretKey'), function(err, decoded) {
+    if (err) {
+      res.json({status:"error", message: err.message, data:null});
+    }else{
+
+      req.body.userId = decoded.id;
+
+      console.log('jwt verify: ' + decoded);
+
+      next();
+    }
+  });
+}
 
 module.exports = app;
