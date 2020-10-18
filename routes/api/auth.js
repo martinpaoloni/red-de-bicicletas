52749e66383f9ef331760bc299ebfd7864d2fee4
Martin Paoloni <martinpaoloni@gmail.com>
2020-10-18 03:20:55 -0300

    Facebook authentication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const passport = require('../../config/passport');
 
-var authController = require('../../controllers/api/authControllerAPI');
+const authController = require('../../controllers/api/authControllerAPI');
 
 router.post('/authenticate', authController.authenticate);
 router.post('/forgotPassword', authController.forgotPassword);
+router.post('/facebook_token', passport.authenticate('facebook-token'), authController.authFacebookToken);
 
 module.exports = router;
