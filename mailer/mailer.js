448e9e5d365a6ca2fa8d60e42ff42111324cb2d8
Martin Paoloni <martinpaoloni@gmail.com>
2020-10-17 22:53:47 -0300

    Configurado SendGrid para envio de mails en production y staging
    Y separadas las credenciales de Ethereal Mail para development en .env.

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,13 +1,38 @@
 const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
 
-const mailConfig = {
-    host: 'smtp.ethereal.email',
-    port: 587,
-    secure: false,
-    auth: {
-        user: 'mohammed.leannon@ethereal.email',
-        pass: 'nXPKqEnaFYHcbdKGBY'
+
+let mailConfig;
+if (process.env.NODE_ENV === 'production'){
+    const options = {
+        auth: {
+            api_key: process.env.SENDGRID_API_SECRET
+        }
     }
-};
+    mailConfig = sgTransport(options);
+
+} else {
+    if (process.env.NODE_ENV === 'staging'){
+        console.log('XXXXXXXXXX');
+        const options = {
+            auth: {
+                api_key: process.env.SENDGRID_API_SECRET
+            }
+        }
+        mailConfig = sgTransport(options);
+
+    } else {
+        // all emails are catched by ethereal.email
+        mailConfig = {
+            host: 'smtp.ethereal.email',
+            port: 587,
+            secure: false,
+            auth: {
+                user: process.env.ethereal_user,
+                pass: process.env.ethereal_pwd
+            }
+        };
+    }
+}
 
 module.exports = nodemailer.createTransport(mailConfig);
